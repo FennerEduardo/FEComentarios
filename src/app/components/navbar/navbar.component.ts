74afeec132fb288afe7ab7b357965b007ea3327a
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2023-10-18 19:23:18 -0500

    :fire: Creating components, interfaces and services

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+ img: string = 'https://evol.com.co/img/logotexto.png';
+}
