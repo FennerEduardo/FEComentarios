0e49482696508d48313d64a4e7baf837522e135c
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2023-10-18 19:46:06 -0500

    :construction: Adding table list

--- src/app/components/list-comentarios/list-comentarios.component.ts
+++ src/app/components/list-comentarios/list-comentarios.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { Comentario } from '../../interfaces/Comentario';
 @Component({
   selector: 'app-list-comentarios',
   templateUrl: './list-comentarios.component.html',
   styleUrls: ['./list-comentarios.component.css']
 })
 export class ListComentariosComponent {
-
+  listComentarios: Comentario[] = [
+    {
+      titulo: 'Hola',
+      creador: 'Fenner',
+      texto: 'comentario',
+      fechaCreacion: new Date()
+    },
+    {
+      titulo: 'Hola de nuevo',
+      creador: 'Fenner',
+      texto: 'comentario',
+      fechaCreacion: new Date()
+    },
+    {
+      titulo: 'Hola de nuevo otra vez',
+      creador: 'Fenner',
+      texto: 'comentario',
+      fechaCreacion: new Date()
+    },
+  ];
 }
